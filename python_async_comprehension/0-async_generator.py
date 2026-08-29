d9d6caa6e86caefd6d90bdcc1560a54d6ccbdc0d
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-08-29 22:06:47 +0300

    Task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """Async generator module."""
-
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yield a random number between 0 and 10 asynchronously."""
     for _ in range(10):
         await asyncio.sleep(1)
