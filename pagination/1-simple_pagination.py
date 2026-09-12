5432b821ddbe28426a38bc4742cb47f368dc1861
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-09-12 14:42:52 +0300

    Task 1

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+This module contains a helper function and a Server class for pagination.
+"""
+import csv
+import math
+from typing import Tuple, List
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple containing a start and end index for pagination.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Gets a specific page of the dataset.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+
+        return data[start:end]
+    
